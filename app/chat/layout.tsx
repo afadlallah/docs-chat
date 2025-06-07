823af40fb5f4b1608ea888e8942bc6debbf10ed3
afadlallah <afadlallah@gmail.com>
2025-06-07 13:40:25 -0400

    Fix sidebar layout

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -12,15 +12,19 @@ export default function ChatLayout({ children }: { children: React.ReactNode }) 
             <Menu className='size-4' />
           </Button>
         </SheetTrigger>
-        <SheetContent className='w-64 p-0' side='left'>
+        <SheetContent className='w-[352px] p-0' side='left'>
           <SheetTitle className='sr-only'>Chat Navigation</SheetTitle>
           <SheetDescription className='sr-only'>Navigation sidebar for chat conversations</SheetDescription>
-          <ChatSidebar />
+          <div className='w-[352px]'>
+            <ChatSidebar />
+          </div>
         </SheetContent>
       </Sheet>
 
-      <div className='hidden w-64 flex-none border-r bg-muted/40 lg:block'>
-        <ChatSidebar />
+      <div className='hidden w-[352px] min-w-[352px] max-w-[352px] flex-none border-r bg-muted/40 lg:block'>
+        <div className='w-[352px]'>
+          <ChatSidebar />
+        </div>
       </div>
 
       <div className='flex-1 overflow-hidden'>{children}</div>
